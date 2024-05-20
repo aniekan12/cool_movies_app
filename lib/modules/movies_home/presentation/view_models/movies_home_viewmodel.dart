8f696f8192615d7a3f8753b4b94aca85b276a522
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2024-05-20 12:43:58 +0100

    add movies home viewmodel

--- lib/modules/movies_home/presentation/view_models/movies_home_viewmodel.dart
+++ lib/modules/movies_home/presentation/view_models/movies_home_viewmodel.dart
@@ -1,11 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-part 'movies_home_event.dart';
-part 'movies_home_state.dart';
+part '../events/movies_home_event.dart';
+part '../states/movies_home_state.dart';
 
-class MoviesHomeBloc extends Bloc<MoviesHomeEvent, MoviesHomeState> {
-  MoviesHomeBloc() : super(MoviesHomeInitial()) {
+class MoviesHomeViewmodel extends Bloc<MoviesHomeEvent, MoviesHomeState> {
+  MoviesHomeViewmodel() : super(MoviesHomeInitial()) {
     on<MoviesHomeEvent>((event, emit) {
       // TODO: implement event handler
     });
