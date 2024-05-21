93e12428baab026e4f7f3cdfa167cab1ae2cbeef
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2024-05-21 22:46:14 +0100

    start building out ui

--- lib/modules/movies_home/presentation/screens/movies_home_screen.dart
+++ lib/modules/movies_home/presentation/screens/movies_home_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:developer';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:coolmovies/common/color/cool_movies_color.dart';
 import 'package:coolmovies/modules/movies_home/presentation/view_models/movies_home_viewmodel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MoviesHomeScreen extends StatefulWidget {
@@ -20,31 +25,72 @@ class _MoviesHomeScreenState extends State<MoviesHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(slivers: [
-        const SliverAppBar(
-          pinned: true,
-          floating: true,
-          snap: true,
-          title: Text("Cool Movies"),
-          centerTitle: true,
-        ),
-        SliverToBoxAdapter(
-          child: BlocConsumer<MoviesHomeViewmodel, MoviesHomeState>(
-            listener: (context, state) {},
-            builder: (context, state) {
-              return switch (state) {
-                MoviesHomeLoading() => const Center(
-                    child: CupertinoActivityIndicator(),
-                  ),
-                MoviesHomeLoaded(movies: final data) =>
-                  Text(data.firstOrNull!.id ?? ""),
-                MoviesHomeError(message: final message) => Text(message),
-                _ => const SizedBox(),
-              };
-            },
+      backgroundColor: CoolMoviesColors.backgroundLight,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              BlocBuilder<MoviesHomeViewmodel, MoviesHomeState>(
+                builder: (context, state) {
+                  if (state is MoviesHomeLoading) {
+                    return const Center(
+                      child: CupertinoActivityIndicator(),
+                    );
+                  }
+                  if (state is MoviesHomeLoaded) {
+                    return Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            CachedNetworkImage(
+                              width: 100,
+                              imageUrl: state.movies.first.imgUrl!,
+                              placeholder: (context, url) =>
+                                  const CupertinoActivityIndicator(),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(state.movies.first.title!),
+                            SizedBox(height: 20),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.calendar_today,
+                                  size: 12,
+                                ),
+                                SizedBox(width: 2),
+                                Text(state.movies.first.releaseDate!),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.person, size: 12),
+                                SizedBox(width: 2),
+                                Text(state
+                                    .movies.first.userByUserCreatorId!.name!),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    );
+                  }
+                  return const SizedBox();
+                },
+              )
+            ],
           ),
-        )
-      ]),
+        ),
+      ),
     );
   }
 }
